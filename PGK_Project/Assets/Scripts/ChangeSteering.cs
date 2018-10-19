36109f04ce4444728efd30f175685b20368e4623
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2018-10-19 21:51:09 +0200

    Project rework, trains

---
+++ PGK_Project/Assets/Scripts/ChangeSteering.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSteering : MonoBehaviour {
+
+	public MoveThePath moveThePath;
+	private int isChange = 0;
+
+	void Start(){
+
+	}
+
+	void OnMouseDown(){
+		if(isChange == 0){
+			//moveThePath.GetComponent<pat
+			isChange=1;
+		}else
+		{
+			isChange=0;
+		}
+
+	}
+
+}
