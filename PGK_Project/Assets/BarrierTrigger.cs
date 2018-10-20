3b0676f7e3df69befd2c29062d726e9bc1d2754a
Piotr Traczyk <pio030195@gmail.com>
2018-10-20 14:40:09 +0200

    Barriers added

---
+++ PGK_Project/Assets/BarrierTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierTrigger : MonoBehaviour {
+
+    public bool isBarrierClosed = false;
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = anim.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if(isBarrierClosed)
+        {
+            isBarrierClosed = false;
+            anim.enabled = true;
+        } else
+        {
+            isBarrierClosed = true;
+            anim.SetTrigger("CloseBarrier");
+        }
+    }
+
+    public void pauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+}
