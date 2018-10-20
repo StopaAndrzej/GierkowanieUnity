22482992cb710764575e66982956976e53d3e827
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2018-10-20 04:12:20 +0200

    trains, change direction

---
+++ PGK_Project/Assets/Scripts/TrainMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainMove : MonoBehaviour {
+
+	public GameObject[] wayPoints;
+	int currentPoint;
+	public float rotationSpeed;
+	public float speed;
+	float wayPointRadius = 0.01f;
+
+	void Update(){
+		if(Vector3.Distance(wayPoints[currentPoint].transform.position, transform.position) < wayPointRadius){
+			currentPoint++;
+			if(currentPoint >= wayPoints.Length){
+				currentPoint =0;
+			}
+		}
+		
+		transform.position = Vector3.MoveTowards(transform.position, wayPoints[currentPoint].transform.position, Time.deltaTime*speed);
+		var rotation = Quaternion.LookRotation (wayPoints [currentPoint].transform.position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+	}
+
+}
