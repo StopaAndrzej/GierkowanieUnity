b9ad2c95ed8688b2923a26c3ef9f8b89711bdfeb
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2018-10-20 22:59:49 +0200

    Camera Control

---
+++ PGK_Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    public float scrollSpeed = 10f;
+    public float minY = 5f;
+    public float maxY = 20f;
+
+    void Update() {
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") ) 
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") )
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") )
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a") )
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * Time.deltaTime *100f;
+        
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+
+        transform.position = pos;
+	}
+}
