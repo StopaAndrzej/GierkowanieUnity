647a6aabfe12cb48de35d822dbbba57907f033c3
Piotr Traczyk <pio030195@gmail.com>
2018-10-23 08:22:46 +0200

    Train collisions

---
+++ PGK_Project/Assets/Scripts/TrainCollider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainCollider : MonoBehaviour
+{
+
+    public TrainMove thisTrain;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Train collided with " + other);
+
+        if (other.transform.tag == "Train")
+        {
+            TrainMove otherTrain = other.GetComponent<TrainMove>();
+            if (otherTrain.speed == 0)
+            {
+                Debug.Log("Collision with train which stopped");
+                thisTrain.lastSpeed = thisTrain.speed;
+                thisTrain.speed = 0;
+            }
+            else
+            {
+                Debug.Log("GAME OVER");
+                Debug.Log("GAME OVER");
+                Debug.Log("GAME OVER");
+                Debug.Log("GAME OVER");
+                Debug.Log("GAME OVER");
+                Debug.Log("GAME OVER");
+                Debug.Log("GAME OVER");
+
+            }
+        }
+
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Train")
+        {
+            thisTrain.speed = thisTrain.lastSpeed;
+        }
+    }
+}
--- PGK_Project/Assets/Scripts/TrainMove.cs
+++ PGK_Project/Assets/Scripts/TrainMove.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrainMove : MonoBehaviour {
+public class TrainMove : MonoBehaviour
+{
 
     public float speed;
-
+    public float lastSpeed;
+    public GameObject StopTrain;
     void Update()
     {
         GetComponent<Rigidbody>().velocity = transform.forward * speed * Time.deltaTime;
     }
-
 }
