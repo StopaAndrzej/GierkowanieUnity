02101b1df365792340d6c53999c7bab282497a47
Piotr Traczyk <pio030195@gmail.com>
2018-11-08 17:33:36 +0100

    Simple game menu and traffic lights(not finished)

---
+++ PGK_Project/Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour {
+
+    public bool isGreen = false;
+    public Material off;
+    public Material greenMaterial;
+    public Material orangeMaterial;
+    public Material redMaterial;
+
+    public GameObject green;
+    public GameObject orange;
+    public GameObject red;
+
+    private int delay = 1;
+
+    void Start () {
+        green.GetComponent<Renderer>().material = off;
+        orange.GetComponent<Renderer>().material = off;
+        red.GetComponent<Renderer>().material = redMaterial;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void OnMouseDown()
+    {
+        if (isGreen)
+        {
+            isGreen = false;
+            green.GetComponent<Renderer>().material = off;
+            orange.GetComponent<Renderer>().material = orangeMaterial;
+            //Invoke("GreenToRed", delay);
+            greenToRed();
+            //red.GetComponent<Renderer>().material = redMaterial;
+        } else
+        {
+            isGreen = true;
+            red.GetComponent<Renderer>().material = off;
+            orange.GetComponent<Renderer>().material = orangeMaterial;
+            //Invoke("RedToGreen", delay);
+            redToGreen();
+
+        }
+
+    }
+
+    public void greenToRed()
+    {
+        orange.GetComponent<Renderer>().material = off;
+        red.GetComponent<Renderer>().material = redMaterial;
+    }
+
+    public void redToGreen()
+    {
+        orange.GetComponent<Renderer>().material = off;
+        green.GetComponent<Renderer>().material = greenMaterial;
+
+    }
+}
---
+++ PGK_Project/Assets/Scripts/TrafficStop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficStop : MonoBehaviour {
+
+    public GameObject trafficLights;
+    public float lastSpeed = 0;
+    public bool isStopped = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!trafficLights.GetComponent<TrafficLight>().isGreen)
+        {
+            if (other.transform.tag == "car")
+            {
+                Debug.Log("STOP CAR");
+                Debug.Log("STOP CAR");
+                Debug.Log("STOP CAR");
+                Debug.Log("STOP CAR");
+                Debug.Log("STOP CAR");
+                Debug.Log("STOP CAR");
+                lastSpeed = other.GetComponent<CarMovement>().moveSpeed;
+                other.GetComponent<CarMovement>().moveSpeed = 0;
+
+                isStopped = true;
+            }
+        }
+    }
+}
