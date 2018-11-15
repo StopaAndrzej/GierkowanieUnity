b4791798dfcc9a35d3c17d3b105205771bfc3ad3
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2018-11-15 02:09:08 +0100

    peopleSpawner

--- PGK_Project/Assets/Scripts/AgentMovement.cs
+++ PGK_Project/Assets/Scripts/AgentMovement.cs
@@ -19,8 +19,8 @@ public class AgentMovement : MonoBehaviour {
         home = GameObject.Find(nameOfPath).transform;
         agent = this.GetComponent<NavMeshAgent>();
         agent.SetDestination(home.position);
-        agent.GetComponent<NavMeshAgent>().speed = 1 + Random.Range(-0.5f, 0.5f);
-        transform.localScale = new Vector3(transform.localScale.x,transform.localScale.y+Random.Range(-0.02f, 0.02f),transform.localScale.z);
+        agent.GetComponent<NavMeshAgent>().speed = 10 + Random.Range(-5f, 5f);
+        transform.localScale = new Vector3(transform.localScale.x*4,transform.localScale.y * 4 + Random.Range(-0.5f, 0.5f),transform.localScale.z*4);
    
 	}
 	
--- PGK_Project/Assets/Scripts/PeopleSpawner.cs
+++ PGK_Project/Assets/Scripts/PeopleSpawner.cs
@@ -11,9 +11,9 @@ public class PeopleSpawner : MonoBehaviour
     public int peopleNumber = 0;
     private int peronCapacity = 100;
     private float maxScaleFactor = 16.7f;
-    public GameObject crowdBar;
-    public GameObject crowdBarRed;
-    public GameObject crowdBarRedHolder;
+    //public GameObject crowdBar;
+    //public GameObject crowdBarRed;
+    //public GameObject crowdBarRedHolder;
     public GameObject parent;
 
     public double crowdBarScaleFactor = 0;
@@ -34,24 +34,24 @@ public class PeopleSpawner : MonoBehaviour
             timer = 0.0f;
             peopleNumber++;
         }
-        if (peopleNumber > 10)
-        {
-            crowdBar.GetComponent<Renderer>().enabled = true;
-            crowdBarRed.GetComponent<Renderer>().enabled = true;
-            Transform t = crowdBarRedHolder.transform;
-            float scale = (float)crowdBarScaleFactor;
-            crowdBarRedHolder.transform.localScale = new Vector3(scale,
-                                                     t.transform.localScale.y,
-                                                     t.transform.localScale.z);
-            if (peopleNumber < peronCapacity)
-            {
-                crowdBarScaleFactor = (float)(peopleNumber * 0.167);
-            }
-        }
-        else
-        {
-            crowdBar.GetComponent<Renderer>().enabled = false;
-            crowdBarRed.GetComponent<Renderer>().enabled = false;
-        }
+        //if (peopleNumber > 10)
+        //{
+        //    crowdBar.GetComponent<Renderer>().enabled = true;
+        //    crowdBarRed.GetComponent<Renderer>().enabled = true;
+        //    Transform t = crowdBarRedHolder.transform;
+        //    float scale = (float)crowdBarScaleFactor;
+        //    crowdBarRedHolder.transform.localScale = new Vector3(scale,
+        //                                             t.transform.localScale.y,
+        //                                             t.transform.localScale.z);
+        //    if (peopleNumber < peronCapacity)
+        //    {
+        //        crowdBarScaleFactor = (float)(peopleNumber * 0.167);
+        //    }
+        //}
+        //else
+        //{
+        //    crowdBar.GetComponent<Renderer>().enabled = false;
+        //    crowdBarRed.GetComponent<Renderer>().enabled = false;
+        //}
     }
 }
