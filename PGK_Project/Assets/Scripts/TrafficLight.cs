619b74a26a1e26948ca9b7ecd3ba4208efddfdc1
Piotr Traczyk <pio030195@gmail.com>
2018-11-20 01:43:20 +0100

    Crossings "almost" working

--- PGK_Project/Assets/Scripts/TrafficLight.cs
+++ PGK_Project/Assets/Scripts/TrafficLight.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TrafficLight : MonoBehaviour {
 
-    public bool isGreen = false;
+    public bool isGreen = true;
     public Material off;
     public Material greenMaterial;
     public Material orangeMaterial;
@@ -14,13 +14,18 @@ public class TrafficLight : MonoBehaviour {
     public GameObject orange;
     public GameObject red;
 
+    private Material greenMaterialOriginal;
+    private Material orangeMaterialOriginal;
+    private Material redMaterialOriginal;
+
     private int delay = 1;
 
     void Start () {
-        green.GetComponent<Renderer>().material = off;
-        orange.GetComponent<Renderer>().material = off;
-        red.GetComponent<Renderer>().material = redMaterial;
+        greenMaterialOriginal = green.GetComponent<Renderer>().material;
+        orangeMaterialOriginal = orange.GetComponent<Renderer>().material;
+        redMaterialOriginal = red.GetComponent<Renderer>().material;
 
+        green.GetComponent<Renderer>().material = greenMaterial;
     }
 
     // Update is called once per frame
@@ -33,33 +38,48 @@ public class TrafficLight : MonoBehaviour {
         if (isGreen)
         {
             isGreen = false;
-            green.GetComponent<Renderer>().material = off;
+            green.GetComponent<Renderer>().material = greenMaterialOriginal;
             orange.GetComponent<Renderer>().material = orangeMaterial;
             //Invoke("GreenToRed", delay);
             greenToRed();
+            enableColliders(false); 
             //red.GetComponent<Renderer>().material = redMaterial;
         } else
         {
+            enableColliders(true);
             isGreen = true;
-            red.GetComponent<Renderer>().material = off;
+            red.GetComponent<Renderer>().material = redMaterialOriginal;
             orange.GetComponent<Renderer>().material = orangeMaterial;
             //Invoke("RedToGreen", delay);
             redToGreen();
-
         }
 
     }
 
     public void greenToRed()
     {
-        orange.GetComponent<Renderer>().material = off;
+        orange.GetComponent<Renderer>().material = orangeMaterialOriginal;
         red.GetComponent<Renderer>().material = redMaterial;
     }
 
     public void redToGreen()
     {
-        orange.GetComponent<Renderer>().material = off;
+        orange.GetComponent<Renderer>().material = orangeMaterialOriginal;
         green.GetComponent<Renderer>().material = greenMaterial;
 
+    }
+
+    private void enableColliders(bool enable)
+    {
+        GameObject trafficStop = GameObject.Find("TrafficLightStop");
+
+        foreach (BoxCollider g in trafficStop.GetComponentsInChildren<BoxCollider>())
+        {
+            Debug.Log(g.tag);
+            if (g.tag == "CrowdBlockade")
+            {
+                g.enabled = enable;
+            }
+        }
     }
 }
