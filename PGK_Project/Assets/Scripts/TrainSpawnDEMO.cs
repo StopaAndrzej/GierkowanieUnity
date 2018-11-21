711ffb8991e2cd2ee2e4144eb890ffcfde604ae7
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2018-11-21 23:51:15 +0100

    DockPackageAnimation, track path to dock, spawnTrain(press TAB)

---
+++ PGK_Project/Assets/Scripts/TrainSpawnDEMO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainSpawnDEMO : MonoBehaviour {
+
+    public GameObject trainToSpawn;
+    public GameObject spawner;
+    public GameObject folderToHoldTrains;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Instantiate(trainToSpawn, spawner.transform.position, Quaternion.Euler(0.0f, 180.0f, 0.0f), folderToHoldTrains.transform);
+        }
+    }
+}
