2585348dc77a6bc9feded78a7263bf87b4a60e1a
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2018-11-25 05:16:17 +0100

    Ticket office panel, buttons still not working

--- PGK_Project/Assets/Scripts/Lecimy Od Nowa/RegisterWindowScript.cs
+++ PGK_Project/Assets/Scripts/Lecimy Od Nowa/RegisterWindowScript.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class RegisterWindowScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject exitButton;
+    public GameObject plusButton;
+    public GameObject minusButton;
+    public TextMeshProUGUI price;
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Klik");
+        }
+
+    }
+
+
+    
 }
