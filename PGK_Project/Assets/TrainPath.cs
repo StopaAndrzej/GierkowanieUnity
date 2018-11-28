b7cb56126b3d206a9b84ffb6a1eb666d1e779c27
Piotr Traczyk <pio030195@gmail.com>
2018-11-28 16:24:48 +0100

    Tracks set up

---
+++ PGK_Project/Assets/TrainPath.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainPath : MonoBehaviour {
+
+    public TrainTrack[] peron1Path;
+    public TrainTrack[] peron2Path;
+    public TrainTrack[] warehousePath;
+}
